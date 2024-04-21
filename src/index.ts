a8c29712043f56642539cf536d0869e77965f4c1
Daniel Silveira <danieldoamaral10@gmail.com>
2024-04-21 14:17:36 +0100

    chore: setup basic express config

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello world!');
+import express, { type Request, type Response } from 'express';
+import helmet from 'helmet';
+
+const PORT = process.env.PORT !== undefined ? Number(process.env.PORT) : 3000;
+
+const app = express();
+
+app.use(helmet());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/public', (_req: Request, res: Response) => {
+  res.send('This is a public resource');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
